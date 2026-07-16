6b70056137a2e0a8dcff8fba0c3920b17af69840
Andressa Caroline de Souza <qa.andressa.souza@gmail.com>
2026-07-16 10:28:36 -0300

    Add Tailwind CSS configuration file
    Configured Tailwind CSS with custom colors and font family.

---
+++ web/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          base: '#2C46B1',
+          dark: '#2C4091',
+        },
+        gray: {
+          100: '#F9F9FB',
+          200: '#E4E6EC',
+          300: '#CDCFD5',
+          400: '#74798B',
+          500: '#4D505C',
+          600: '#1F2025',
+        },
+        danger: '#B12C4D',
+      },
+      fontFamily: {
+        sans: ['Open Sans', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
